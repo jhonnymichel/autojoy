3a281fb75b067952c68b44f525873d30dbd54cec
Jhonny Michel <jhonnymichel@outlook.com>
2025-07-29 01:40:34 -0400

    support for CRKD guitar on PC mode (RPCS3 and Dolphin)

--- src/autojoy-backend/joystick.mjs
+++ src/autojoy-backend/joystick.mjs
@@ -4,6 +4,42 @@ import {
   joystickTypes,
 } from "../common/joystick.mjs";
 
+export function createJoystick(raw, mode) {
+  switch (mode) {
+    case joystickModes.xinput:
+      return createJoystickFromXinputDevice(raw);
+    case joystickModes.sdl:
+      return createJoystickFromSDLDevice(
+        raw,
+        getHardwareInfo({
+          manufacturerId: raw.vendor,
+          productId: raw.product,
+        })
+      );
+    default:
+      console.error("[Joystick Creator] mode not supported:", mode);
+      break;
+  }
+}
+
+export function isHardware(
+  { manufacturerId, productId } = {},
+  hardwareInfoEntry
+) {
+  return (
+    hardwareInfoEntry?.manufacturerId === manufacturerId &&
+    hardwareInfoEntry?.productId === productId
+  );
+}
+
+export function getHardwareInfo(deviceInfo) {
+  return Object.values(hardwareInfo).find(
+    (hw) =>
+      hw.manufacturerId === deviceInfo.manufacturerId &&
+      hw.productId === deviceInfo.productId
+  );
+}
+
 const xinputSubtypeToGlobalType = {
   XINPUT_DEVSUBTYPE_GUITAR_BASS: joystickTypes.guitar,
   XINPUT_DEVSUBTYPE_GUITAR: joystickTypes.guitar,
@@ -17,47 +53,27 @@ function getXinputJoystickType(device) {
   return xinputSubtypeToGlobalType[device.subType];
 }
 
-export function isHardware(
-  { manufacturerId, productId } = {},
-  hardwareInfoEntry
-) {
-  return (
-    hardwareInfoEntry?.manufacturerId === manufacturerId &&
-    hardwareInfoEntry?.productId === productId
-  );
-}
-
-function getSDLJoystickType(device) {
-  const deviceInfo = {
-    manufacturerId: device.vendor,
-    productId: device.product,
-  };
-
-  if (isHardware(deviceInfo, hardwareInfo.guitarHeroGuitarForPS3)) {
-    return joystickTypes.ps3GuitarHeroGuitar;
+function getSDLJoystickType(device, hardwareInfo) {
+  // Device-specific handlers.
+  // not all devices need this. a generic joystick type exists further down this function.
+  if (hardwareInfo) {
+    return hardwareInfo.type;
   }
 
-  // TODO: This is misleading. further diferentiation might be needed.
+  // TODO: This is a catch-all generic matcher. further diferentiation might be needed.
   // right now, this works well for x360 and santroller guitars only.
   if (device.name.includes("Guitar")) return joystickTypes.sdlGuitar;
-  if (
-    isHardware(deviceInfo, hardwareInfo.harmonixDrumControllerForNintendoWii) ||
-    isHardware(deviceInfo, hardwareInfo.harmonixDrumControllerForPS3)
-  ) {
-    return joystickTypes.wiiRockBandDrumKit;
-  }
-  // TODO: This is misleading, further diferentiation might be needed.
+  // TODO: This is catch-all generic matcher. further diferentiation might be needed.
   // right now, this works well for x360 Rock Band 2 and 3 Drum Kit.
-  // TODO: Support PS kits, similar to how we're supporting wii kits.
   if (device.name.includes("Drum")) return joystickTypes.sdlRockBandDrumKit;
 
   return joystickTypes.gamepad;
 }
 
-function createJoystickFromSDLDevice(device) {
+function createJoystickFromSDLDevice(device, hardwareInfo) {
   return {
-    name: device.name,
-    type: getSDLJoystickType(device),
+    name: hardwareInfo.name ?? device.name,
+    type: getSDLJoystickType(device, hardwareInfo),
     raw: device,
   };
 }
@@ -68,16 +84,4 @@ function createJoystickFromXinputDevice(device) {
     type: getXinputJoystickType(device),
     raw: device,
   };
-}
-
-export function createJoystick(raw, mode) {
-  switch (mode) {
-    case joystickModes.xinput:
-      return createJoystickFromXinputDevice(raw);
-    case joystickModes.sdl:
-      return createJoystickFromSDLDevice(raw);
-    default:
-      console.error("[Joystick Creator] mode not supported:", mode);
-      break;
-  }
 }
--- src/common/joystick.mjs
+++ src/common/joystick.mjs
@@ -7,6 +7,7 @@ export const joystickTypes = {
   guitar: "GUITAR",
   sdlGuitar: "SDL_GUITAR",
   ps3GuitarHeroGuitar: "PS3_GUITARHERO_GUITAR",
+  crkdGuitarPCMode: "CRKD_GUITAR_PC_MODE",
   rockBandDrumKit: "ROCKBAND_DRUM_KIT",
   sdlRockBandDrumKit: "SDL_ROCKBAND_DRUM_KIT",
   wiiRockBandDrumKit: "WII_ROCKBAND_DRUM_KIT",
@@ -17,13 +18,22 @@ export const hardwareInfo = {
   harmonixDrumControllerForNintendoWii: {
     manufacturerId: 7085,
     productId: 12560,
+    type: joystickTypes.wiiRockBandDrumKit,
   },
   harmonixDrumControllerForPS3: {
     manufacturerId: 4794,
     productId: 528,
+    type: joystickTypes.wiiRockBandDrumKit,
   },
   guitarHeroGuitarForPS3: {
     manufacturerId: 4794,
     productId: 256,
+    type: joystickTypes.ps3GuitarHeroGuitar,
+  },
+  crkdGuitarPCMode: {
+    manufacturerId: 1118,
+    productId: 654,
+    type: joystickTypes.crkdGuitarPCMode,
+    name: "CRKD Guitar (PC Mode)",
   },
 };
