f06688fd160b9cb5623b6c3a324250701a24ffa6
Jhonny Costa <jhonnymichel@outlook.com>
2025-12-12 03:33:40 -0500

    fixing "already running" error box display

--- src/main/index.js
+++ src/main/index.js
@@ -2,14 +2,14 @@ import { app, dialog } from "electron";
 
 const gotTheLock = app.requestSingleInstanceLock();
 
-if (!gotTheLock) {
-  dialog.showErrorBox(
-    "Autojoy is already running!",
-    "Another instance of Autojoy is already running. Close other instances before opening the app.",
-  );
-  app.quit();
-} else {
-  app.on("ready", async () => {
+app.on("ready", async () => {
+  if (!gotTheLock) {
+    dialog.showErrorBox(
+      "Autojoy is already running!",
+      "Another instance of Autojoy is already running. Close other instances before opening the app.",
+    );
+    app.quit();
+  } else {
     import("./startup.mjs");
-  });
-}
+  }
+});
