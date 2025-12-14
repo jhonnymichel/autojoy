ed0d0a647ce3bfbc65d4fd65d990887aba4469b7
Jhonny Costa <jhonnymichel@outlook.com>
2025-12-14 06:58:36 -0500

    wii u support on linux

--- src/autojoy-backend/integrations/cemu.mjs
+++ src/autojoy-backend/integrations/cemu.mjs
@@ -1,10 +1,13 @@
 import path from "path";
 import { deleteFile, loaders, savers } from "../../common/file.mjs";
-import { findNextConnectedXinputIdentifier } from "./shared.mjs";
+import {
+  findNextConnectedXinputIdentifier,
+  getFixedOldDeviceSDLName,
+} from "./shared.mjs";
 import { user } from "../../common/settings.mjs";
 import { createJoystickFromXinputDevice } from "../joystick.mjs";
 
-const configTemplates = loaders.xml("config-templates/cemu.xml");
+const configTemplates = loaders.xml("config-templates/cemu.xml").configs;
 const cemuPath = path.resolve(user.paths.cemu, "controllerProfiles");
 const inputConfigFileNames = [
   "controller0.xml",
@@ -14,8 +17,26 @@ const inputConfigFileNames = [
 ];
 
 const xinputApiValue = "XInput";
+const sdlApiValue = "SDLController";
+
+const wiiUGamepadType = "Wii U GamePad";
+const wiiUProControllerType = "Wii U Pro Controller";
 
 const xinputDisplayNamePrefix = "Controller";
+
+// Ceumu adds a <index>_ before the UUID.
+// the number is relative to how many of the same controller is connected. it's not a player/position indicator.
+function prependNumbersToSDLDeviceUUID(arr) {
+  const counts = {};
+  return arr.map((item) => {
+    if (counts.hasOwnProperty(item.raw.guid)) {
+      counts[item.raw.guid] += 1;
+    } else {
+      counts[item.raw.guid] = 0;
+    }
+    return { ...item, uuid: `${counts[item.raw.guid]}_${item.raw.guid}` };
+  });
+}
 
 function handleXinputJoystickListUpdate(joystickList) {
   let trimmedList = joystickList.filter((joystick) => joystick);
@@ -26,7 +47,7 @@ function handleXinputJoystickListUpdate(joystickList) {
       console.log(
         "CEMU - Deleting input Settings for controller",
         position + 1,
-        path.resolve(cemuPath, filename)
+        path.resolve(cemuPath, filename),
       );
       deleteFile(path.resolve(cemuPath, filename));
       return;
@@ -34,7 +55,7 @@ function handleXinputJoystickListUpdate(joystickList) {
 
     const deviceIndex = findNextConnectedXinputIdentifier(
       joystickList,
-      position
+      position,
     );
 
     const device = joystickList[deviceIndex];
@@ -52,13 +73,55 @@ function handleXinputJoystickListUpdate(joystickList) {
       "CEMU - Input Settings for controller",
       position + 1,
       "saved at",
-      path.resolve(cemuPath, filename)
+      path.resolve(cemuPath, filename),
     );
   });
 }
 
-async function handleSDLJoystickListUpdate() {
-  // we don't actually support SDL, so we'll just spin up xinput here real quick to support cemu.
+async function handleSDLJoystickListUpdate(joystickList) {
+  if (process.platform === "linux") {
+    const deviceList = prependNumbersToSDLDeviceUUID(joystickList);
+    inputConfigFileNames.forEach((filename, position) => {
+      if (!deviceList[position]) {
+        console.log(
+          "CEMU - Deleting input Settings for controller",
+          position + 1,
+          path.resolve(cemuPath, filename),
+        );
+        deleteFile(path.resolve(cemuPath, filename));
+        return;
+      }
+
+      const device = deviceList[position];
+      let newConfig;
+      if (position === 0) {
+        newConfig = configTemplates.WII_U_GAMEPAD;
+
+        newConfig.emulated_controller.controller[1].api = sdlApiValue;
+        newConfig.emulated_controller.controller[1].uuid = device.uuid;
+        newConfig.emulated_controller.controller[1].display_name =
+          getFixedOldDeviceSDLName(device);
+      } else {
+        newConfig = configTemplates.GAMEPAD;
+
+        newConfig.emulated_controller.controller.api = sdlApiValue;
+        newConfig.emulated_controller.controller.uuid = device.uuid;
+        newConfig.emulated_controller.controller.display_name =
+          getFixedOldDeviceSDLName(device);
+      }
+
+      savers.xml(newConfig, path.resolve(cemuPath, filename));
+      console.log(
+        "CEMU - Input Settings for controller",
+        position + 1,
+        "saved at",
+        path.resolve(cemuPath, filename),
+      );
+    });
+    return;
+  }
+
+  // we don't actually support SDL on windows, so we'll just spin up xinput here real quick to support cemu.
   const xinput = await import("xinput-ffi");
   const deviceList = [];
 
